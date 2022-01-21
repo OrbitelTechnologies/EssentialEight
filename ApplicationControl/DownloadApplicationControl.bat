0a09bbca50709d2d67a5dc72bd39e998bb6394b4
Jerome Ryan <jerome@orbitel.com.au>
2022-01-21 17:09:42 +1030

    Added Application Control Downloader

--- ApplicationControl/DownloadApplicationControl.bat
+++ ApplicationControl/DownloadApplicationControl.bat
@@ -1,8 +1,9 @@
 REM Created by Jerome Ryan 20/01/2022 for Orbitel Technologies
 
-mkdir C:\Orbitel\Scripts\E8
+mkdir C:\Orbitel\Scripts\E8\WDAC
 
-powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/541882f6132d84a1537628188660e02c3c53f29f/OfficeMacroDisable.bat" -OutFile C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat" }"
-SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\OfficeMacroDisable" /F /TR C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat /RU Users
-
-pause
+powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/Policy.zip" -OutFile "C:\Orbitel\Scripts\E8\WDAC\Policy.zip"}"
+powershell -command "& { Expand-Archive "C:\Orbitel\Scripts\E8\WDAC\Policy.zip" -DestinationPath "C:\Orbitel\Scripts\E8\WDAC\" -Force }"
+powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/DeployWdac.ps1" -OutFile "C:\Orbitel\Scripts\E8\WDAC\DeployWdac.ps1" }"
+powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/RefreshPolicy(AMD64).exe" -OutFile "C:\Orbitel\Scripts\E8\WDAC\RefreshPolicy(AMD64).exe" }"
+SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\DeployWdac" /F /TR C:\Orbitel\Scripts\E8\WDAC\DeployWdac.ps1 /RU Users
