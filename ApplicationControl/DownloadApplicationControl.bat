fee68e48af1e2ef9bb34b46bcee04648d0ab0a06
Jerome Ryan <jerome@orbitel.com.au>
2022-01-21 17:19:45 +1030

    rename

--- ApplicationControl/DownloadApplicationControl.bat
+++ ApplicationControl/DownloadApplicationControl.bat
@@ -3,7 +3,9 @@ REM Created by Jerome Ryan 20/01/2022 for Orbitel Technologies
 mkdir C:\Orbitel\Scripts\E8\WDAC
 
 powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/Policy.zip" -OutFile "C:\Orbitel\Scripts\E8\WDAC\Policy.zip"}"
-powershell -command "& { Expand-Archive "C:\Orbitel\Scripts\E8\WDAC\Policy.zip" -DestinationPath "C:\Orbitel\Scripts\E8\WDAC\" -Force }"
+powershell -command "& { Expand-Archive "C:\Orbitel\Scripts\E8\WDAC\Policy.zip" -DestinationPath "C:\Orbitel\Scripts\E8\WDAC\Policy" -Force }"
 powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/DeployWdac.ps1" -OutFile "C:\Orbitel\Scripts\E8\WDAC\DeployWdac.ps1" }"
-powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/RefreshPolicy(AMD64).exe" -OutFile "C:\Orbitel\Scripts\E8\WDAC\RefreshPolicy(AMD64).exe" }"
+powershell -command "& { Invoke-Webrequest "https://github.com/OrbitelTechnologies/EssentialEight/blob/bcb2048971032dc3b0969b2022622dd10c1b8472/ApplicationControl/WDAC/RefreshPolicyAMD64.exe" -OutFile "C:\Orbitel\Scripts\E8\WDAC\RefreshPolicyAMD64.exe" }"
 SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\DeployWdac" /F /TR C:\Orbitel\Scripts\E8\WDAC\DeployWdac.ps1 /RU Users
+
+pause
