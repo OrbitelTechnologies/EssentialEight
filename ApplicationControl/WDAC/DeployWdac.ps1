10a6089c331d67d08e006bea1f3335a67b553c66
Jerome Ryan <jerome@orbitel.com.au>
2022-01-21 16:45:59 +1030

    cleanup

---
+++ ApplicationControl/WDAC/DeployWdac.ps1
@@ -0,0 +1,4 @@
+﻿Copy-Item -Path "C:\Orbitel\Scripts\E8\WDAC\{126B0167-8054-4F24-9024-B8023B70F707}.cip" -Destination $env:windir+"\System32\CodeIntegrity\CIPolicies\Active\" -Force
+
+& "C:\Orbitel\Scripts\E8\WDAC\RefreshPolicy(AMD64).exe"
+
