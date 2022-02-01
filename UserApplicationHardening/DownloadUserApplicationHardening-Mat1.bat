822d0a61d793679dccebded6b2fd5dc89d3217c9
Jerome Ryan <jerome@orbitel.com.au>
2022-02-01 17:08:09 +1030

    Signed-off-by: Jerome Ryan <jerome@orbitel.com.au>

---
+++ UserApplicationHardening/DownloadUserApplicationHardening-Mat1.bat
@@ -0,0 +1,6 @@
+REM Created by Jerome Ryan 20/01/2022 for Orbitel Technologies
+
+mkdir C:\Orbitel\Scripts\E8
+
+powershell -command "& { Invoke-Webrequest "https://raw.githubusercontent.com/OrbitelTechnologies/EssentialEight/main/ConfigureMicrosoftOfficeMacroSettings/OfficeMacroDisable.bat" -OutFile C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat" }"
+SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\OfficeMacroDisable" /F /TR C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat /RU Users /RL HIGHEST
---
+++ UserApplicationHardening/UserApplicationHardening-Mat1.ps1
@@ -0,0 +1,15 @@
+﻿# Jerome Ryan - Orbitel Technologies - 21/12/2021
+# This script was created to bring users in line with Essential Eight User Application Hardening Maturity Level 1.
+
+#Installs chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#Installs Adblocker for Chrome and Firefox
+C:\ProgramData\chocolatey\choco.exe install ublockorigin-chrome -y
+C:\ProgramData\chocolatey\choco.exe install adblockplus-firefox -y
+
+#Disables Java in Web browser.
+(Get-Content C:\Users\$env:USERNAME\AppData\LocalLow\Sun\Java\Deployment\deployment.properties).replace('deployment.webjava.enabled=true', 'deployment.webjava.enabled=false') | Set-Content C:\Users\$env:USERNAME\AppData\LocalLow\Sun\Java\Deployment\deployment.properties
+
+#Uninstalls Internet Explorer 11. This doesn't work on Windows 11 becuase it doesn't have IE.
+Disable-WindowsOptionalFeature -online -FeatureName internet-explorer-optional-amd64
