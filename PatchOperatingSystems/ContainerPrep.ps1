a9bcd9bbc72ecf10ea97ece65019ef86f6857c9d
Jerome Ryan <jerome@orbitel.com.au>
2022-02-03 13:19:22 +1030

    Signed-off-by: Jerome Ryan <jerome@orbitel.com.au>

--- PatchOperatingSystems/ContainerPrep.ps1
+++ PatchOperatingSystems/ContainerPrep.ps1
@@ -1,6 +1,15 @@
 ﻿# Jerome Ryan - Orbitel Technologies - 03/02/2021
 #Enables Containers as a windows feature, then downloads and installs Docker Desktop.
 
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
 Install-WindowsFeature -Name 'Containers'
 
 #Installs chocolatey
@@ -10,5 +19,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 C:\ProgramData\chocolatey\choco.exe choco install docker-desktop
 
 
-Read-Host -Prompt "Reboot required! Press any key to continue"
+Read-Host -Prompt "Reboot required! Press any key to continue (Won't reboot)"
 
--- PatchOperatingSystems/InstallContainer.ps1
+++ PatchOperatingSystems/InstallContainer.ps1
@@ -1,22 +1,52 @@
 ﻿# Jerome Ryan - Orbitel Technologies - 03/02/2021
-# Set's up the OpenVAS container
+# Sets up the OpenVAS container
+
+
+Write-Host "OpenVAS Container setup script. Created by Jerome Ryan for Orbitel Technologies"
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
 
 $password = Read-Host -Prompt "Enter a secure password for OpenVAS" 
 $port = Read-Host -Prompt "Enter Port (Default is 8080)"
+$port = -join($port,":9392")
 
+Write-Host "Setting to Windows Engine mode. Please wait..."
 #Ensures Linux Daemon installed by switching between both.
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchWindowsEngine
 Start-Sleep -Seconds 60
+Write-Host "Setting to Linux Engine mode. Please wait..."
 & $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchLinuxEngine
 Start-Sleep -Seconds 60
 
+Write-Host "Creating container..."
 #Start container with persistant volume
-docker run –restart:always --detach --publish ($port):9392 -e PASSWORD=$password --volume openvas:/data --name openvas immauss/openvas
+docker run --detach --publish $port -e PASSWORD=$password --volume openvas:/data --name openvas immauss/openvas | Out-Null
+Start-Sleep -Seconds 1
+Write-Host "Starting Container..."
 docker start openvas
+Start-Sleep -Seconds 1
+Write-Host "Setting container to start with operating system..."
+docker update --restart unless-stopped openvas
+Start-Sleep -Seconds 1
 
 
 #Makes directory and downloads maintenance file into it.
 # Creates a scheduled script to run this everyday at 6am
-mkdir C:\Orbitel\Docker
+Write-Host "Creating C:\Orbitel\Docker directory..."
+mkdir C:\Orbitel\Docker -ErrorAction SilentlyContinue
+Start-Sleep -Seconds 1
+Write-Host "Downloading maintenance script to C:\Orbitel\Docker..."
 Invoke-Webrequest "https://raw.githubusercontent.com/OrbitelTechnologies/EssentialEight/main/PatchOperatingSystems/DockerMaintenanceOpenVAS.ps1" -OutFile C:\Orbitel\Docker\DockerMaintenanceOpenVAS.ps1
-cmd.exe /c "SCHTASKS /CREATE /st 06:00 /TN "Orbitel\PatchOperatingSystems" /F /TR "%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file C:\Orbitel\Docker\DockerMaintenanceOpenVAS.ps1" /RU SYSTEM /RL HIGHEST"
+Start-Sleep -Seconds 1
+Write-Host "Creating scheduled task to run daily at 6am..."
+cmd.exe /c "SCHTASKS /CREATE /sc daily /st 06:00 /TN "Orbitel\DockerMaintenance" /F /TR "%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file C:\Orbitel\Docker\DockerMaintenanceOpenVAS.ps1" /RU SYSTEM /RL HIGHEST"
+
+
+Read-Host -Prompt "Completed. Press any key to continue."
