7465c974e1042331e174056a0eb79d0e60552594
Jerome Ryan <jerome@orbitel.com.au>
2022-02-03 21:55:12 +1030

    Signed-off-by: Jerome Ryan <jerome@orbitel.com.au>

--- ConfigureMicrosoftOfficeMacroSettings/DownloadOfficeMacroProtect.bat
+++ ConfigureMicrosoftOfficeMacroSettings/DownloadOfficeMacroProtect.bat
@@ -3,5 +3,5 @@ REM Created by Jerome Ryan 20/01/2022 for Orbitel Technologies
 @echo off
 mkdir C:\Orbitel\Scripts\E8
 
-powershell -command "& { Invoke-Webrequest "https://raw.githubusercontent.com/OrbitelTechnologies/EssentialEight/main/ConfigureMicrosoftOfficeMacroSettings/OfficeMacroDisable.bat" -OutFile C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat" }"
-SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\OfficeMacroDisable" /F /TR C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat /RU INTERACTIVE /RL HIGHEST
+powershell -command "& { Invoke-Webrequest "https://raw.githubusercontent.com/OrbitelTechnologies/EssentialEight/main/ConfigureMicrosoftOfficeMacroSettings/OfficeMacroProtect.bat" -OutFile C:\Orbitel\Scripts\E8\OfficeMacroDisable.bat" }"
+SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\OfficeMacroProtect" /F /TR C:\Orbitel\Scripts\E8\OfficeMacroProtect.bat /RU INTERACTIVE /RL HIGHEST
