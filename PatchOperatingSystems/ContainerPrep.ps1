a1802ae32cd158a78c61f66d6bf51195076b7f40
Jerome Ryan <jerome@orbitel.com.au>
2022-02-03 11:38:02 +1030

    Signed-off-by: Jerome Ryan <jerome@orbitel.com.au>

---
+++ PatchOperatingSystems/ContainerPrep.ps1
@@ -0,0 +1,14 @@
+﻿# Jerome Ryan - Orbitel Technologies - 03/02/2021
+#Enables Containers as a windows feature, then downloads and installs Docker Desktop.
+
+Install-WindowsFeature -Name 'Containers'
+
+#Installs chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#Installs Docker Desktop
+C:\ProgramData\chocolatey\choco.exe choco install docker-desktop
+
+
+Read-Host -Prompt "Reboot required! Press any key to continue"
+
---
+++ PatchOperatingSystems/DockerMaintenanceOpenVAS.ps1
@@ -0,0 +1,3 @@
+﻿docker pull immauss/openvas
+start-sleep -Seconds 3600
+docker restart 
