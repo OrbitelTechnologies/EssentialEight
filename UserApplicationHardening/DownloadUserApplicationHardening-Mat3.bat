7912748b7971932b714f83fae4d787a6db30438e
Jerome Ryan <jerome@orbitel.com.au>
2022-02-06 21:07:40 +1030

    Signed-off-by: Jerome Ryan <jerome@orbitel.com.au>

---
+++ UserApplicationHardening/DownloadUserApplicationHardening-Mat3.bat
@@ -0,0 +1,7 @@
+REM Created by Jerome Ryan 20/01/2022 for Orbitel Technologies
+
+@echo off
+mkdir C:\Orbitel\Scripts\E8
+
+powershell -command "& { Invoke-Webrequest "https://raw.githubusercontent.com/OrbitelTechnologies/EssentialEight/main/UserApplicationHardening/UserApplicationHardening-Mat1.ps3" -OutFile C:\Orbitel\Scripts\E8\UserApplicationHardening-Mat3.ps1" }"
+SCHTASKS /CREATE /SC ONLOGON /TN "Orbitel\UserApplicationHardening\MaturityThree" /F /TR C:\Orbitel\Scripts\E8\UserApplicationHardening-Mat3.ps1 /RU INTERACTIVE /RL HIGHEST
